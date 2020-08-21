99797d6358421cf5c9c72182867e13724f809216
Hommer <lih1989@yandex.ru>
2020-08-21 23:18:22 +0300

    Отключение создания файла со стилями

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/'
         : '/vue-input-calculator/',
-
     outputDir: process.env.NODE_ENV === 'production'
         ? __dirname+'/lib'
-        : __dirname+'/demo'
+        : __dirname+'/demo',
+    css: {extract: false}
 };
