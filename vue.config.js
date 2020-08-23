b01691e8066d01dfaacd52ed22c94c85fe70b6f0
Hommer <lih1989@yandex.ru>
2020-08-23 16:41:10 +0300

    version++

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'development'
+    publicPath: process.env.NODE_ENV === 'production'
         ? '/'
         : '/vue-input-calculator/',
     outputDir: process.env.NODE_ENV === 'production'
         ? __dirname+'/lib'
         : __dirname+'/demo',
     css: {
-        extract: false
+        extract: process.env.NODE_ENV !== 'production'
     }
 };
